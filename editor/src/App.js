73700e276fd52d4b5b4bbc5530ea524bfd1d0bb9
Lortus64 <adei18@student.bth.se>
2021-09-01 17:50:36 +0200

    first submit

---
+++ editor/src/App.js
@@ -0,0 +1,12 @@
+import './style/App.css';
+import CKeditor from './components/CKeditor';
+
+function App() {
+  return (
+    <div className="App">
+      <CKeditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ editor/src/components/CKeditor.js
@@ -0,0 +1,40 @@
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import '../style/bar.css';
+
+const CKeditor = () => {
+    var saveData= "";
+    function save() {
+        console.log(saveData);
+    };
+
+    return (
+        <div className="editor">
+            <div className="bar">
+                <button onClick={save}>Save</button>
+            </div>
+            
+            <CKEditor
+                editor={ ClassicEditor }
+                data=""
+                onReady={ editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+                onChange={ ( event, editor ) => {
+                    const data = editor.getData();
+                    console.log( { event, editor, data } );
+                    saveData = data;
+                } }
+                onBlur={ ( event, editor ) => {
+                    console.log( 'Blur.', editor );
+                } }
+                onFocus={ ( event, editor ) => {
+                    console.log( 'Focus.', editor );
+                } }
+            />
+        </div>
+    );
+}
+
+export default CKeditor;
